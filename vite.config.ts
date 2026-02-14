80fb0710f68d19cbac028e7399bc149b3e654c7e
jakelong00 <jakelong00@users.noreply.github.com>
2026-02-15 00:22:14 +0530

    Update vite.config.ts
    changed ./ to /nihongo-flow/

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Essential for GitHub Pages (Project Pages)
+  base: '/nihongo-flow/', // Essential for GitHub Pages (Project Pages)
 })
